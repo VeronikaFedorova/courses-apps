ac1e72dd8b9aa213415722a8b489b1b0ec3ef573
VeronikaFedorova <fedorovavv03@gmail.com>
2022-10-25 04:41:01 +0300

    1hw

--- src/components/Courses/components/SearchBar/SearchBar.jsx
+++ src/components/Courses/components/SearchBar/SearchBar.jsx
@@ -2,20 +2,11 @@ import React from 'react';
 import './SearchBar.css';
 import Button from '../../../../common/Button/Button';
 import Input from '../../../../common/Input/Input';
-import { mockedCoursesList } from '../../../../constants';
 
 const SearchBar = () => {
-	function performSearch(e) {
-		//const courses = mockedCoursesList.map((course) => course.toLowerCase());
-		console.log(e);
-	}
 	return (
 		<div className='searchbar'>
-			<Input
-				labelText='search'
-				placeholderText='Enter course name'
-				onChange={performSearch()}
-			/>
+			<Input labelText='search' placeholderText='Enter course name' />
 			<Button buttonText='Search' />
 		</div>
 	);
